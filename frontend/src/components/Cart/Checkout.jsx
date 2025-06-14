6c56e6a6445bb27d1f123e5faa22cac290f671d0
Muhammad Usman <shiekhusman492@gmail.com>
2025-06-14 14:34:13 +0500

    Fix import casing: paypal upadte name

--- frontend/src/components/Cart/Checkout.jsx
+++ frontend/src/components/Cart/Checkout.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import PayPalButton from './PaypalButton';
+import PayPalButton from './PayPalButton';
 import { useDispatch, useSelector } from 'react-redux';
 import { createCheckout } from '../../redux/slices/checkoutSlice';
 import axios from "axios";
